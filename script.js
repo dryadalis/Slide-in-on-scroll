e4aca884c197aca97a1263b02e10a0d7b22198e5
Karolina <kika.romanska@gmail.com>
2018-01-12 16:18:00 +0100

    SLide in at effect added, layout changed, little red riding hood story added

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-// Limits the rate at witch function can fire
+//  Limits the rate at witch function can fire
 function debounce(func, wait = 20, immediate = true) {
-    let timeout;
+    var timeout;
     return function() {
-        let context = this, args = arguments;
-        let later = function() {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        let callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if(callNow) func.apply(context, args);
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
     };
-};
-
-
-const sliderImages = document.querySelectorAll('.slide-in');
-
-function checkSlide(e) {
-    console.count(e);
-}
-window.addEventListener('scroll', debounce(checkSlide));
+  };
+  const sliderImages = document.querySelectorAll('.slide-in');
+  function checkSlide() {
+    sliderImages.forEach(sliderImage => {
+      // the pixel level of when each of the images should be slide in at - HALF WAY THROUGHT THE IMAGE
+      const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+      // the pixel level of how far down it bottom is - BOTTOM OF THE IMAGE
+      const imageBottom = sliderImage.offsetTop + sliderImage.height;
+      const isHalfShown = slideInAt > sliderImage.offsetTop;
+      const isNotScrolledPast = window.scrollY < imageBottom;
+      if (isHalfShown && isNotScrolledPast) {
+        sliderImage.classList.add('active');
+      } else {
+        sliderImage.classList.remove('active');
+      }
+    });
+  }
+  window.addEventListener('scroll', debounce(checkSlide));
